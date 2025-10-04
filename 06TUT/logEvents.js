ae28285ca2a14630e2be2ebfb2a2bed95bb820c6
marion586 <marionmenye@gmail.com>
2025-10-04 07:15:47 +0300

    introducation to express

---
+++ 06TUT/logEvents.js
@@ -0,0 +1,24 @@
+console.log('testing!')
+const {format} = require('date-fns')
+const {v4: uuid} = require('uuid')
+
+
+const fs = require('fs')
+const fsPromises = require('fs').promises
+const path = require('path')
+
+
+const logEvents = async (message,filename)=> {
+    const dateTime = format(new Date(), 'yyyy-MM-dd\tHH:mm:ss')
+    const logItem = `${dateTime}\t${uuid()}\t${message}\n`
+    console.log(logItem)
+    try {
+        if(!fs.existsSync(path.join(__dirname, 'logs'))) {
+            await fsPromises.mkdir(path.join(__dirname, 'logs'))
+        }
+        await fsPromises.appendFile(path.join(__dirname, 'logs' , filename), logItem);
+    } catch (error) {
+        console.log(error)
+    }
+}
+module.exports = logEvents
---
+++ 06TUT/server.js
@@ -0,0 +1,47 @@
+
+const path = require('path')
+const express = require('express')
+const app = express()
+
+
+app.get(/^\/$|index(.html)?/,(req, res)=> {
+    res.sendFile(path.join(__dirname, "views", "index.html"))
+    //or res.sendFile('./views/index.html', {root: __dirname})
+})
+app.get('/new-page.html',(req, res)=> {
+    res.sendFile(path.join(__dirname, "views", "new-page.html"))
+})
+app.get(/\/old-page(.html)?/,(req, res)=> {
+    res.redirect(301,'/new-page.html') //302 by default
+})
+
+//route handlers
+app.get(/\/hello(.html)?/,(req, res, next)=> {
+    console.log('attempted to load hello.html')
+    next()
+} , (req, res)=> {
+    res.send('Hello World')
+})
+
+
+const PORT = process.env.PORT || 3500   
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+
+
+const one = (req, res, next) => {
+    console.log('one')
+    next()
+}
+const two = (req, res, next) => {
+    console.log('two')
+    next()
+}
+const three = (req, res, next) => {
+    console.log('three')
+    res.send('Finished!')
+}
+app.get(/\/chain(.html)?/, [one, two, three])
+
+app.get(/\/*/,(req, res)=> {
+    res.status(404).sendFile(path.join(__dirname, "views", "404.html"))
+})
